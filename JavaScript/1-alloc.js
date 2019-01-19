aea86447f80e86ca7a64975f075b0444ded8176c
tshemsedinov <timur.shemsedinov@gmail.com>
2019-01-20 01:23:54 +0200

    Improve examples

--- JavaScript/1-alloc.js
+++ JavaScript/1-alloc.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const b1 = Buffer.alloc(1024);
+console.log({ isBuffer: Buffer.isBuffer(b1) });
 console.log(b1);
 
 const b2 = Buffer.allocUnsafe(1024);
+console.log({ isBuffer: Buffer.isBuffer(b2) });
 console.log(b2);
+
--- JavaScript/4-methods.js
+++ JavaScript/4-methods.js
@@ -3,10 +3,11 @@
 const buffer = Buffer.from('Marcus Aurelius');
 
 if (buffer.includes('Marcus')) {
-  console.log(`"${buffer.toString()}" includes "Marcus"`);
+  console.log(`"${buffer}" includes "Marcus"`);
 }
 
 const k = buffer.indexOf('Aurelius');
 console.log(`Index of "Aurelius" is ${k}`);
 
 console.log(`Slice 3-5 "${buffer.slice(3, 6)}"`);
+
